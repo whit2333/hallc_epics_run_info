b6ad5de441d0e80dafa4ea0a9c4d1f093224d439
Whitney Armstrong <warmstrong@anl.gov>
2018-11-02 14:32:43 -0400

    Added all build files 	new file:   Makefile 	new file:   configure/CONFIG 	new file:   configure/CONFIG_SITE 	new file:   configure/Makefile 	new file:   configure/RELEASE 	new file:   configure/RULES 	new file:   configure/RULES.ioc 	new file:   configure/RULES_DIRS 	new file:   configure/RULES_TOP 	new file:   iocBoot/Makefile 	new file:   iocBoot/ioctest1/Makefile 	new file:   iocBoot/ioctest1/README 	new file:   iocBoot/ioctest1/st.cmd 	modified:   test1App/Db/hallc.db 	modified:   test1App/Db/user.substitutions 	new file:   test1App/Makefile 	new file:   test1App/src/Makefile 	new file:   test1App/src/dbSubExample.c 	new file:   test1App/src/dbSubExample.dbd 	new file:   test1App/src/devXxxSoft.c 	new file:   test1App/src/devtest1Version.c 	new file:   test1App/src/devtest1Version.dbd 	new file:   test1App/src/initTrace.c 	new file:   test1App/src/initTrace.dbd 	new file:   test1App/src/sncExample.dbd 	new file:   test1App/src/sncExample.stt 	new file:   test1App/src/sncProgram.st 	new file:   test1App/src/test1Hello.c 	new file:   test1App/src/test1Hello.dbd 	new file:   test1App/src/test1Main.cpp 	new file:   test1App/src/xxxRecord.c 	new file:   test1App/src/xxxRecord.dbd 	new file:   test1App/src/xxxSupport.dbd

---
+++ iocBoot/ioctest1/st.cmd
@@ -0,0 +1,35 @@
+#!../../bin/linux-x86_64/test1
+
+#- You may have to change test1 to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/test1.dbd"
+test1_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadTemplate "db/user.substitutions"
+
+#dbLoadRecords "db/test1Version.db", "user=whit"
+
+dbLoadRecords "db/hallc.db"
+dbLoadRecords "db/daq_run.db", "runtype=COIN"
+dbLoadRecords "db/daq_run.db", "runtype=SHMS"
+dbLoadRecords "db/daq_run.db", "runtype=HMS"
+dbLoadRecords "db/run_settings.db"
+
+#- Set this to see messages from mySub
+#var mySubDebug 1
+
+#- Run this to trace the stages of iocInit
+#traceIocInit
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncExample, "user=whit"
