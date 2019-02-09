58499df897287781b43af3627f236fcf3519de82
Whitney Armstrong <warmstrong@anl.gov>
2019-02-09 13:18:25 -0500

    modified:   iocBoot/ioctest1/st.cmd 	modified:   monitor_all.sh

--- monitor_all.sh
+++ monitor_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-camonitor                                \
+pvmonitor                                \
 hcStartNewRunSetting                     \
 hcCOINIntRunNumber                       \
 hcSHMSIntRunNumber                       \
